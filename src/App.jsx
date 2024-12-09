b1bb45f9cdb3a3cd4a8f5500ed3d31817a7ff708
sujahathmsm <sujahathmhmd@gmail.com>
2024-12-09 21:31:46 +0530

    Props - Children

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import Card from "./Components/Card";
+import './App.css'
 const App = () => {
-  return <div>Hello React HuXn - Sujahath</div>;
+  return (
+    <div className="container">
+      <h1>Hello React HuXn - Sujahath</h1>
+      <Card>
+        <h2>Hello Card - 01</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi,
+          nostrum.
+        </p>
+      </Card>
+      <Card>
+        <h2>Hello Card - 02</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi,
+          nostrum.
+        </p>
+      </Card>
+      <Card>
+        <h2>Hello Card - 03</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi,
+          nostrum.
+        </p>
+      </Card>
+    </div>
+  );
 };
 
 export default App;
