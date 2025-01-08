843f632e735bd847becf1263f7c1a27040c20052
sujahathmsm <sujahathmhmd@gmail.com>
2025-01-08 20:07:59 +0530

    Custom hooks and unique ID - useId

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { AiFillCode } from "react-icons/ai";
 import Refer from "./components/Useref/Refer";
 import Timer from "./components/Useref/Timer";
 import TimeRef from "./components/Useref/TimeRef";
+import Chook from "./components/Customhooks/Chook";
+import UseID from "./components/Customhooks/UseID";
 const App = () => {
   return (
     <div className="container">
@@ -11,9 +13,15 @@ const App = () => {
         <AiFillCode style={{ fontSize: 50, color: "gold" }} />
         Hello React HuXn - Sujahath
       </h1>
-      <Refer />
+      {/* <Refer />
       <Timer />
-      <TimeRef/>
+      <TimeRef />
+      <Chook /> */}
+      <UseID />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+      aspernatur tempore cumque reprehenderit temporibus minima magnam facere
+      debitis minus animi.
+      <UseID />
     </div>
   );
 };
